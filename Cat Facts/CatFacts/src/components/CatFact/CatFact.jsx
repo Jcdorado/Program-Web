f2122c9fb4483dce5c81914e3f3dfc0631809bdc
Jcdorado <88757034+Jcdorado@users.noreply.github.com>
2023-10-06 22:25:27 -0500

    Add files via upload

---
+++ Cat Facts/CatFacts/src/components/CatFact/CatFact.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import './catfact.css'
+export function CatFact({text}) {
+    return (
+        <p>{text}</p>
+    )
+}
---
+++ Cat Facts/CatFacts/src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import './header.css'
+export function Header() {
+    return(
+        <h1>Wanna know something funny about cats ?</h1>
+    )
+}
